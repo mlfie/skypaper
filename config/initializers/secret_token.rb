f7a020b9bb2b772357d0c69a40ffcb01b8b4f004
syokenz <syokenz@gmail.com>
2012-01-15 22:19:28 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skypaper::Application.config.secret_token = '8810cb0e2e29d79d62c0ec3fb062199b9e4fd4aacee989c88951bef47ac4e8d40eb2bc700f2a294b882628881887a64f6a36b109e5f30656d1967f2341d9a070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Skypaper::Application.config.session_store :cookie_store, key: '_skypaper_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Skypaper::Application.config.session_store :active_record_store
